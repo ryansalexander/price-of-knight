875be282a06ce0b95c707ccdcb2143184c3de908
Ryan Alexander <alexander.ryanscott@gmail.com>
2025-06-08 19:53:55 -0400

    Initial Commit

---
+++ backend/scripts/scryfall.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request, render_template
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+
+    # POST request
+    if request.method == 'POST':
+        print('Incoming..')
+        print(request.get_json())  # parse as JSON
+        return 'OK', 200
+
+    # GET request
+    else:
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
